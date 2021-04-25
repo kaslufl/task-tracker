bb1fbb3fd45aac0b9aad8f10b2376d295bbc80c4
kaslufl <leallucasfonseca@gmail.com>
2021-04-25 00:37:02 -0300

    feat: adiciona addTask, formata código usando Prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
-    const onClick = (e) => {
-       console.log('click') 
-    }
+  const onClick = (e) => {
+    console.log("click");
+  };
 
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker 2021',
-}
+  title: "Task Tracker 2021",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
